ba68e71ace50caf150830bb6102521414d027c8f
llcesselx <108751430+llcesselx@users.noreply.github.com>
2022-09-25 18:00:30 -0400

    Create concatenation.js

---
+++ concatenation.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+let monthNames = [
+"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",
+];
+
+let monthNumber = 9;
+let name = "L T";
+
+console.log("My name is " + name + ", and my birthday is in the month of " + monthNames[monthNumber-1] + ".");
+
