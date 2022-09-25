25ffc2ce94711d7e8c56996836dcf0bda2caa321
llcesselx <108751430+llcesselx@users.noreply.github.com>
2022-09-25 17:59:13 -0400

    Create arrayConcat.js

---
+++ arrayConcat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+let data = [
+    "Lea",
+    "Tice",
+    "lt@syc.ind.ed",
+];
+
+console.log("My name is " + data[0] + " " + data[1] + ", and my email address is " + data[2] + ".");
+console.log();
+console.log(data[data.length-1]);
+
